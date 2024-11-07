9eb9216c4d9f493fa771bde43aa1e7352edbec7a
timow-gh <39085531+timow-gh@users.noreply.github.com>
2024-11-07 22:42:22 +0100

    Delete test directories.

--- test/XDGBaseDirectoriesTest.cpp
+++ test/XDGBaseDirectoriesTest.cpp
@@ -3,6 +3,12 @@
 
 class XDGBaseDirectoriesTest : public ::testing::Test {
 protected:
+  void remove_test_dir(std::filesystem::path testDir)
+  {
+    std::filesystem::remove_all(testDir);
+    ASSERT_FALSE(std::filesystem::exists(testDir));
+  }
+
   // cppcheck-suppress-begin unusedStructMember
   std::string m_testAuthor{"testAuthor"};
   std::string m_testAppName{"pillar_test_app"};
@@ -11,22 +17,26 @@ protected:
 
 TEST_F(XDGBaseDirectoriesTest, get_config_home)
 {
-  [[maybe_unused]] auto path = pillar::xdg::get_config_home(m_testAuthor, m_testAppName);
+  auto path = pillar::xdg::get_config_home(m_testAuthor, m_testAppName);
+  remove_test_dir(path);
 }
 
 TEST_F(XDGBaseDirectoriesTest, get_data_home)
 {
-  [[maybe_unused]] auto path = pillar::xdg::get_data_home(m_testAuthor, m_testAppName);
+  auto path = pillar::xdg::get_data_home(m_testAuthor, m_testAppName);
+  remove_test_dir(path);
 }
 
 TEST_F(XDGBaseDirectoriesTest, get_cache_home)
 {
-  [[maybe_unused]] auto path = pillar::xdg::get_cache_home(m_testAuthor, m_testAppName);
+  auto path = pillar::xdg::get_cache_home(m_testAuthor, m_testAppName);
+  remove_test_dir(path);
 }
 
 TEST_F(XDGBaseDirectoriesTest, get_state_home)
 {
-  [[maybe_unused]] auto path = pillar::xdg::get_state_home(m_testAuthor, m_testAppName);
+  auto path = pillar::xdg::get_state_home(m_testAuthor, m_testAppName);
+  remove_test_dir(path);
 }
 
 TEST_F(XDGBaseDirectoriesTest, get_runtime_dir)
